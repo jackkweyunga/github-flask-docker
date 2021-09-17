ccaf3add95550040c792d91f17e7427690fb6560
jackkweyunga <jackkweyunga@gmail.com>
2021-09-17 03:33:58 +0300

    bug fix

--- copy.sh
+++ copy.sh
@@ -4,6 +4,8 @@
 
 # scp -i $SSH_KEY $SSH_USERNAME@$SSH_HOST:/home/$SSH_USERNAME/
 
+echo $SSH_KEY > key
 
-rsync -avz -i $SSH_KEY ./flask-git-docker/ $SSH_USERNAME@$SSH_HOST:/home/$SSH_USERNAME/flask-git-docker/
+rsync -avz -i key ./flask-git-docker/ $SSH_USERNAME@$SSH_HOST:/home/$SSH_USERNAME/flask-git-docker/
 
+rm key
